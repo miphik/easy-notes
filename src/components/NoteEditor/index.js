6ffd849c594be75a12319c170f8ecb820f434443
Andrei Savchuk <andrei.savchuk@n26.com>
2020-01-16 15:44:03 +0100

    update styles

--- src/components/NoteEditor/index.js
+++ src/components/NoteEditor/index.js
@@ -4,78 +4,14 @@ import debounce from 'lodash/debounce';
 import {inject, observer} from 'mobx-react';
 import PropTypes from 'prop-types';
 import * as React from 'react';
-import {
-    EditorState, convertToRaw, convertFromRaw, Modifier, convertFromHTML, ContentState,
-} from 'draft-js';
 import ScrollableColumn from 'components/ScrollableColumn';
 import isEqual from 'lodash/isEqual';
 import moment from 'moment';
-import memoizeOne from 'memoize-one';
-import type {ThemeType} from 'stores/ThemeStore';
-import Editor, {composeDecorators} from 'draft-js-plugins-editor';
-import createStore from 'components/Plug/utils/createStore';
-import {defaultTheme} from 'components/Plug/theme';
-import Toolbar from 'components/Plug/components/Toolbar';
-import createImagePlugin from 'draft-js-image-plugin';
-import createAlignmentPlugin from 'draft-js-alignment-plugin';
-import createFocusPlugin from 'draft-js-focus-plugin';
-import createResizeablePlugin from 'draft-js-resizeable-plugin';
-import createBlockDndPlugin from 'draft-js-drag-n-drop-plugin';
-import createSideToolbarPlugin from 'draft-js-side-toolbar-plugin';
-import {stateFromHTML} from 'draft-js-import-html';
 import JoditEditor from 'jodit-react';
 import Radium, {Style} from 'radium';
+import {STYLES, getTextStyles} from './styles';
 import styles from './styles.styl';
 
-const focusPlugin = createFocusPlugin();
-const resizeablePlugin = createResizeablePlugin();
-const blockDndPlugin = createBlockDndPlugin();
-const alignmentPlugin = createAlignmentPlugin();
-const {AlignmentTool} = alignmentPlugin;
-const sideToolbarPlugin = createSideToolbarPlugin();
-const {SideToolbar} = sideToolbarPlugin;
-
-const decorator = composeDecorators(
-    resizeablePlugin.decorator,
-    alignmentPlugin.decorator,
-    focusPlugin.decorator,
-    blockDndPlugin.decorator,
-);
-const imagePlugin = createImagePlugin({decorator});
-
-const inlineToolbar = (config = {}) => {
-    const store = createStore({
-        isVisible: false,
-    });
-
-    const {theme = defaultTheme} = config;
-
-    return {
-        initialize: ({getEditorState, setEditorState, getEditorRef}) => {
-            store.updateItem('getEditorState', getEditorState);
-            store.updateItem('setEditorState', setEditorState);
-            store.updateItem('getEditorRef', getEditorRef);
-        },
-        // Re-Render the text-toolbar on selection change
-        onChange: editorState => {
-            store.updateItem('selection', editorState.getSelection());
-            return editorState;
-        },
-        InlineToolbar: props => <Toolbar {...props} store={store} theme={theme}/>,
-    };
-};
-
-const inlineToolbarPlugin = inlineToolbar();
-const {InlineToolbar} = inlineToolbarPlugin;
-const plugins = [
-    blockDndPlugin,
-    focusPlugin,
-    alignmentPlugin,
-    resizeablePlugin,
-    imagePlugin,
-    inlineToolbarPlugin,
-    sideToolbarPlugin,
-];
 
 const config = {
     showPlaceholder:      false,
@@ -91,57 +27,6 @@ const config = {
 };
 
 const toolbarClassName = 'NoteText__toolbar';
-const STYLES = memoizeOne((theme: ThemeType) => (
-    {
-        headers: {
-            color: '#8e8e8e',
-        },
-        joditPopupTriangle: {
-            backgroundColor: '#8e8e8e !important',
-        },
-        toolbar: {
-            color: theme.color.gray,
-        },
-        joditBtn: {
-            color:           '#d3d3d3 !important',
-            backgroundColor: '#1c262a !important',
-            border:          '1px solid #747474',
-        },
-        joditToolbarBtn: {
-            color: '#8e8e8e',
-        },
-        joditTabBtn: {
-            backgroundColor: 'rgba(211, 211, 211, 0.2) !important',
-        },
-        joditInserterAfter: {
-            color: '#d3d3d3',
-        },
-        joditToolbarHover: {
-            color: '#d3d3d3',
-        },
-        joditTooltipHover2: {
-            fill: '#d3d3d3 !important',
-        },
-        joditTooltipHover: {
-            backgroundColor: '#1c262a !important',
-        },
-        joditToolbar: {
-            backgroundColor: '#1c262a !important',
-        },
-        joditToolbar3: {
-            border: '1px solid #747474',
-        },
-        joditTooltip: {
-            padding:         '0.3em 0.5em !important',
-            borderRadius:    '0.2em !important',
-            backgroundColor: `${theme.color.second} !important`,
-            fontSize:        '0.8em !important',
-            border:          `1px solid ${theme.color.gray}`,
-            color:           `${theme.color.buttonActive} !important`,
-            textAlign:       'center',
-        },
-    }
-));
 
 @inject(stores => (
     {
@@ -155,14 +40,6 @@ const STYLES = memoizeOne((theme: ThemeType) => (
 @observer
 @Radium
 class NoteEditor extends React.Component {
-    static propTypes = {
-        name: PropTypes.string,
-    };
-
-    static defaultProps = {
-        name: '',
-    };
-
     static getDerivedStateFromProps(nextProps, prevState) {
         if (!nextProps.selectedNote) return {currentNote: null, currentNoteText: null};
         if (!prevState.currentNote
@@ -171,9 +48,9 @@ class NoteEditor extends React.Component {
             const state = {currentNote: nextProps.selectedNote};
             if ((!prevState.currentNoteText || nextProps.selectedNote.uuid !== prevState.currentNote.uuid)
                 && nextProps.noteText && nextProps.noteText.entityMap) {
-                state.currentNoteText = EditorState.createWithContent(CustomContentStateConverter(convertFromRaw(nextProps.noteText)));
+                state.currentNoteText = nextProps.noteText;
             } else if (!prevState.currentNoteText) {
-                state.currentNoteText = EditorState.createEmpty();
+                state.currentNoteText = '';
             }
             return state;
         }
@@ -185,10 +62,8 @@ class NoteEditor extends React.Component {
         const {noteText} = props;
         this.state = {
             content:         '',
-            currentNoteText: noteText && noteText.entityMap
-                ? EditorState.createWithContent(convertFromRaw(noteText))
-                : EditorState.createEmpty(),
-            currentNote: props.selectedNote,
+            currentNoteText: noteText,
+            currentNote:     props.selectedNote,
         };
     }
 
@@ -197,59 +72,21 @@ class NoteEditor extends React.Component {
     onChangeNote = data => {
         const {selectedNote, selectedCategory} = this.props;
         this.setState({
-            currentNoteText: EditorState.set(
-                data,
-                {currentContent: CustomContentStateConverter(data.getCurrentContent())},
-            ),
-            currentNote: selectedNote,
+            currentNoteText: data,
+            currentNote:     selectedNote,
         }, () => {
-            if (!this.props.selectedNote.text
-                || !isEqual(this.props.selectedNote.text.blocks, convertToRaw(data.getCurrentContent()).blocks)) {
-                this.debounceChangeNoteText(selectedNote, selectedCategory, convertToRaw(data.getCurrentContent()));
+            if (!this.props.selectedNote.text || !isEqual(this.props.selectedNote.text, data)) {
+                this.debounceChangeNoteText(selectedNote, selectedCategory, data);
             }
         });
     };
-
-    handlePastedText = (text: string, html?: string): boolean => {
-        const contentState = stateFromHTML(html);
-        const blocksFromHTML = convertFromHTML(html || text);
-        const state = ContentState.createFromBlockArray(
-            blocksFromHTML.contentBlocks,
-            blocksFromHTML.entityMap,
-        );
-
-        console.log(1111, state, html);
-        // if they try to paste something they shouldn't let's handle it
-        if (text.indexOf('text that should not be pasted') !== -1) {
-            // we'll add a message for the offending user to the editor state
-            const newContent = Modifier.insertText(
-                this.state.editorState.getCurrentContent(),
-                this.state.editorState.getSelection(),
-                'nice try, chump!',
-            );
-
-            // update our state with the new editor content
-            this.onChange(EditorState.push(
-                this.state.editorState,
-                newContent,
-                'insert-characters',
-            ));
-            return true;
-        }
-        this.onChange(EditorState.createWithContent(
-            CustomContentStateConverter(state),
-            decoratorR,
-        ));
-        return true;
-    };
-
-    onChange = editorState => this.setState({editorState});
 
     render() {
         const {selectedNote, offset, theme} = this.props;
         const {currentNoteText, content} = this.state;
         const showComponent = currentNoteText !== null && selectedNote;
         const style = STYLES(theme);
+        const textStyle = getTextStyles(style);
         return (
             <ScrollableColumn
                 autoHideScrollbar
@@ -270,25 +107,7 @@ class NoteEditor extends React.Component {
             >
                 {showComponent ? (
                     <>
-                        <Style rules={{
-                            '.jodit_form_inserter .jodit_form-table-creator-box.jodit_form-container>div.hovered:after':                             style.joditInserterAfter,
-                            '.jodit_dark_theme .jodit_tabs .jodit_tabs_buttons>a':                                                                   style.joditTabBtn,
-                            '.jodit_dark_theme .jodit_toolbar li.jodit_toolbar_btn>a':                                                               style.joditToolbarBtn,
-                            '.jodit_dark_theme .jodit_toolbar li.jodit_toolbar_btn:hover>a':                                                         style.joditToolbarHover,
-                            '.jodit_toolbar .jodit_toolbar .jodit_toolbar':                                                                          style.joditToolbar3,
-                            '.jodit_toolbar > li:not(.jodit_disabled):hover>a svg':                                                                  style.joditTooltipHover2,
-                            '.jodit_tooltip':                                                                                                        style.joditTooltip,
-                            '.jodit_toolbar':                                                                                                        style.joditToolbar,
-                            '.jodit_popup_triangle':                                                                                                 style.joditPopupTriangle,
-                            '.jodit_button':                                                                                                         style.joditBtn,
-                            '.jodit_statusbar':                                                                                                      style.joditToolbar,
-                            '.jodit_toolbar >.jodit_toolbar_btn.jodit_active, .jodit_toolbar > .jodit_toolbar_btn:not(.jodit_toolbar-input):active': style.joditTabBtn,
-                            '.jodit_active':                                                                                                         style.joditTabBtn,
-                            'h1, h2, h3, h4, h5, h6':                                                                                                style.headers,
-                            'h1:hover, h2:hover, h3:hover, h4:hover, h5:hover, h6:hover':                                                            style.joditToolbarHover,
-                            '.jodit_toolbar > .jodit_toolbar_btn:not(.jodit_toolbar-input):hover':                                                   style.joditTooltipHover,
-                        }}
-                        />
+                        <Style rules={textStyle}/>
                         <JoditEditor
                             // ref={editor}
                             value={content}
@@ -307,28 +126,3 @@ class NoteEditor extends React.Component {
 }
 
 export default NoteEditor;
-
-export const CustomContentStateConverter = contentState => {
-    // changes block type of images to 'atomic'
-    const newBlockMap = contentState.getBlockMap().map(block => {
-        const entityKey = block.getEntityAt(0);
-        if (entityKey !== null) {
-            const entityBlock = contentState.getEntity(entityKey);
-            const entityType = entityBlock.getType();
-            switch (entityType) {
-                case 'IMAGE': {
-                    const newBlock = block.merge({
-                        type: 'atomic',
-                        text: 'img',
-                    });
-                    return newBlock;
-                }
-                default:
-                    return block;
-            }
-        }
-        return block;
-    });
-    const newContentState = contentState.set('blockMap', newBlockMap);
-    return newContentState;
-};
--- src/stores/ThemeStore.js
+++ src/stores/ThemeStore.js
@@ -12,6 +12,7 @@ export type ThemeType = {
     },
     color: {
         gray: string,
+        selected: string,
         button: string,
         buttonActive: string,
         dangerButton: string,
@@ -38,6 +39,8 @@ const DEFAULT_THEME = (scale: number) => (
         },
         color: {
             gray: '#747474',
+
+            selected: 'rgba(211, 211, 211, 0.2)',
 
             button:       '#8e8e8e',
             buttonActive: '#d3d3d3',
